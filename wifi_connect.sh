eb3f352d175b0d37ea7a67b5395d60dd17b7f01d
Jason Caballero (Rousemary) <jasonadonayvlz@gmail.com>
2025-12-10 18:48:20 -0600

    Add files via upload

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+clear
 
 echo ""
 
-echo -e "\033[33m¡Script para establecer una coneccion a internet Con Network Manager!\e[0m"
+echo -e "\e[1;35m$(cat log)\e[0m"
+
+#echo -e "\033[33m                               Repo Manager\033[0m"
+echo ""
+echo "[-] Create by:                    |         whoamijas0n (Rousemary)"
+echo "[-] Version:                      |         1.0.0"
+echo "[-] Github:                       |         https://github.com/whoamijas0n"
 
 echo ""
 
-echo -e "\033[34mBy Rousemary\033[0m"
-echo ""
-
-echo -e "\033[31m==> Lista de redes wifi disponibles...\033[0m"
+echo -e "\033[34m[-]  Lista de redes wifi disponibles...\033[0m"
 
 echo ""
 
@@ -17,7 +21,7 @@ nmcli dev wifi list
 
 echo ""
 
-read -p "Escribe el nombre de la red a la que deseas conectarte: " red
+read -p $'\e[33m[-] Escribe el SSID O BSSID de la red a la que deseas conectarte: \e[0m ' red
 
 echo ""
 
@@ -26,3 +30,5 @@ nmcli dev wifi connect $red --ask
 echo ""
 
     echo -e "\033[33m¡¡Ten un buen dia!!\e[0m"
+
+echo ""
